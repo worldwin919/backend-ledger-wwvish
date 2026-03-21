21d63698b4337421474c6713ddff822678814def
worldwin919 <nayalvishvajeet@gmail.com>
2026-03-21 16:45:21 +0530

    added transaciton controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -27,4 +27,58 @@ async function createTransaction(req, res) {
   }
 
   //continue from 2:13:13
+  //1. valdiate request
+  const fromUserAccount = await accountModel.findOne({
+    _id: fromAccount,
+  });
+
+  const toUserAccount = await accountModel.findOne({
+    _id: toAccount,
+  });
+
+  if (!fromUserAccount || !toUserAccount) {
+    return res.status(404).json({
+      message: "One or both accounts not found",
+    });
+  }
+
+  //2. validate idempotency key
+  const transactionExists = await transactionModel.findOne({
+    idempotencyKey: idempotencyKey,
+  });
+
+  if (transactionExists) {
+    if (transactionExists.status == "COMPLETED") {
+      return res.status(200).json({
+        message: "Transaction already completed",
+        transaction: transactionExists,
+      });
+    }
+
+    if (transactionExists.status == "PENDING") {
+      return res.status(200).json({
+        message: "Transaction is pending",
+        transaction: transactionExists,
+      });
+    }
+
+    if (transactionExists.status == "FAILED") {
+      return res.status(200).json({
+        message: "Transaction failed",
+        transaction: transactionExists,
+      });
+    }
+
+    if (transactionExists.status == "REVERSED") {
+      return res.status(500).json({
+        message: "Transaction was reversed, please try again",
+      });
+    }
+  }
+
+  /**
+   * 3. check account status
+   */
+
+  
 }
